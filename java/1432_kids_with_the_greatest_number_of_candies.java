37dab3b7589f9e84ea3b40a2bf1c15c3d63bbcda
Lochlann O Neill <lochlannjoneill@gmail.com>
2023-09-16 03:45:47 +0100

    update 'faster' to 'better'

--- java/1432_kids_with_the_greatest_number_of_candies.java
+++ java/1432_kids_with_the_greatest_number_of_candies.java
@@ -1,6 +1,6 @@
 // ALL TESTS PASSED
-// Runtime: 1ms (faster than 89.59%)
-// Memory: 41.60MB (faster than 32.90%)
+// Runtime: 1ms (better than 89.59%)
+// Memory: 41.60MB (better than 32.90%)
 // 
 // There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
 // Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
