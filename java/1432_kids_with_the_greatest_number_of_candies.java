05e89a9e2bd070b3659973d7539e718283e157b2
Lochlann O Neill <lochlannjoneill@gmail.com>
2023-09-18 00:14:56 +0100

    Improve readability

--- java/1432_kids_with_the_greatest_number_of_candies.java
+++ java/1432_kids_with_the_greatest_number_of_candies.java
@@ -23,7 +23,7 @@ class Solution {
         int max = 0;
 
         for (int i = 0; i < candies.length; i++) {
-            max = (candies[i] > max) ? candies[i] : max;
+            max = (max > candies[i]) ? max : candies[i];
         }
 
         for (int i = 0; i < candies.length; i++) {
