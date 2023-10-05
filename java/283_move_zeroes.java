7d91fea407a576263e5bb42fa183974c0377ac39
Lochlann O Neill <lochlannjoneill@gmail.com>
2023-10-05 04:06:15 +0100

    Create 283_move_zeroes.java

---
+++ java/283_move_zeroes.java
@@ -0,0 +1,32 @@
+// ALL TESTS PASSED
+// Time Complexity: O(n)
+// Runtime: 1ms (better than 100.00%)
+// Memory: 45.49MB (better than 8.28%)
+// 
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+//
+// Note that you must do this in-place without making a copy of the array.
+//
+// Example 1:
+//      Input: nums = [0,1,0,3,12]
+//      Output: [1,3,12,0,0]
+// Example 2:
+//      Input: nums = [0]
+//      Output: [0]
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int index_nozero = 0;
+
+        for (int num : nums) {
+            if (num != 0) {
+                nums[index_nozero] = num;
+                index_nozero++;
+            }
+        }
+
+        for (int i = index_nozero; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
