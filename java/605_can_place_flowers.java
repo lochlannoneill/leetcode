82d29761fd8ad40528eb10495b77c140c3cbb797
Lochlann O Neill <lochlannjoneill@gmail.com>
2024-01-21 20:36:55 +0000

    minor - syntax

--- java/605_can_place_flowers.java
+++ java/605_can_place_flowers.java
@@ -21,13 +21,13 @@ class Solution {
         int count = 0;
         for (int i = 0; i < flowerbed.length; i++) {
             if(flowerbed[i] != 0) continue;
-                int left = (i == 0) ? 0 : flowerbed[i-1];
-                int right = (i == flowerbed.length - 1) ? 0 : flowerbed[i+1];
+            int left = (i == 0) ? 0 : flowerbed[i-1];
+            int right = (i == flowerbed.length - 1) ? 0 : flowerbed[i+1];
 
-                if ((left == 0) && (right == 0)) {
-                    flowerbed[i] = 1;
-                    count++;
-                }
+            if ((left == 0) && (right == 0)) {
+                flowerbed[i] = 1;
+                count++;
+            }
         }
 
         return n <= count;
