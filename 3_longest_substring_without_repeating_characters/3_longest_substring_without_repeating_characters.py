a6a226092c650840fab8b0bee55b8abfa35b84ca
Lochlann O Neill <lochlannjoneill@gmail.com>
2025-09-27 15:56:04 +0100

    completed question

---
+++ 3_longest_substring_without_repeating_characters/3_longest_substring_without_repeating_characters.py
@@ -0,0 +1,54 @@
+# 3. Longest Substring Without Repeating Characters
+# Solved
+# Medium
+
+# Given a string s, find the length of the longest substring without duplicate characters.
+
+ 
+
+# Example 1:
+
+# Input: s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+# Example 2:
+
+# Input: s = "bbbbb"
+# Output: 1
+# Explanation: The answer is "b", with the length of 1.
+# Example 3:
+
+# Input: s = "pwwkew"
+# Output: 3
+# Explanation: The answer is "wke", with the length of 3.
+# Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+# Constraints:
+
+# 0 <= s.length <= 5 * 104
+# s consists of English letters, digits, symbols and spaces.
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        left = 0
+        right = 0
+        max_substring = 0
+        substring = list()
+
+        while right < len(s):
+            # right pointer + 1
+            if s[right] not in substring:
+                substring.append(s[right])
+                right += 1
+                max_substring = max(max_substring, len(substring))  # compare max at each iteration
+            # left pointer + 1
+            else:
+                substring.remove(s[left])
+                left += 1
+
+        return max_substring
