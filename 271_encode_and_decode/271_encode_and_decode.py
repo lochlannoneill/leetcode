d7168ddb59ab12901537a830b2ba24e7782e9782
Lochlann O Neill <lochlannjoneill@gmail.com>
2025-09-28 17:08:14 +0100

    Create 271_encode_and_decode.py

---
+++ 271_encode_and_decode/271_encode_and_decode.py
@@ -0,0 +1,25 @@
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        res = []
+        for string in strs:
+            # res += str(len(string)) + "#" + string
+            res.append(str(len(string)) + "#" + string)
+        return "".join(res)
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i:j])
+            
+            word_start = j+1
+            word_end = j+length+1
+            res.append(s[word_start:word_end])
+
+            i = word_end
+
+        return res
+
