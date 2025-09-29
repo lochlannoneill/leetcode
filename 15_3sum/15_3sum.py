8697656cc786580048aa66ff1e8a1a62d17c3745
Lochlann O Neill <lochlannjoneill@gmail.com>
2025-09-29 03:17:07 +0100

    Update 15_3sum.py

--- 15_3sum/15_3sum.py
+++ 15_3sum/15_3sum.py
@@ -45,15 +45,15 @@ class Solution(object):
         nums.sort()
 
         for anchor in range(len(nums)):
-            if anchor > 0 and nums[anchor] == nums[anchor-1]:  # Skip duplicate anchor
+            # Skip duplicate anchor, check if over 1 to avoid becoming [-1]
+            if anchor > 0 and nums[anchor] == nums[anchor-1]:
                 continue
-
-            left = anchor + 1
-            right = len(nums) - 1
-
+            
+            left, right = anchor + 1, len(nums) - 1
             while left < right:
                 sum_of_pointers = nums[anchor] + nums[left] + nums[right]
 
+                # Add successful sum list to result
                 if sum_of_pointers == 0:
                     res.append([nums[anchor], nums[left], nums[right]])
                     while left < right and nums[left] == nums[left+1]:
@@ -62,9 +62,13 @@ class Solution(object):
                         right -= 1
                     left += 1
                     right -= 1
+
+                # Needs bigger sum
                 elif sum_of_pointers < 0:
                     left += 1
+
+                # Needs smaller sum
                 elif sum_of_pointers > 0:
                     right -= 1
-        
+
         return res
