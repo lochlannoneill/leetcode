fb1c18d5d51a6f14eed22cbefc9e8a9225c5dac1
Lochlann O Neill <lochlannjoneill@gmail.com>
2025-09-29 14:03:23 +0100

    Update 11_container_with_most_water.py

--- 11_container_with_most_water/11_container_with_most_water.py
+++ 11_container_with_most_water/11_container_with_most_water.py
@@ -37,18 +37,18 @@ class Solution(object):
         :rtype: int
         """        
         left = 0
-        right = len(heights) - 1
+        right = len(height) - 1
         max_area = 0
 
         while left < right:
             # Get current area and compare to max_area
-            length = min(heights[left], heights[right])
+            length = min(height[left], height[right])
             width = right - left
             area = length * width
             max_area = max(max_area, area)
 
             # Update pointers
-            if heights[left] <= heights[right]:
+            if height[left] <= height[right]:
                 left += 1
             else:
                 right -= 1
